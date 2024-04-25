20f0e30e76ad14a97c86c6b1f8ef1e555b78208f
Teresa <teresabroyer2003@gmail.com>
2024-04-25 11:30:57 -0500

    delete user

--- daniel/crud.js
+++ daniel/crud.js
@@ -1,70 +1,128 @@
 
 
-
 const btnPost = document.getElementById("post")
 const formHTML = document.getElementById("form")
 const listaHTML = document.getElementById("lista-usuario")
 
 let listaUsuarios = [
-{
-    name:"pepito", email:"pepito@gmail.com"
-},
-{
-    name:"lucia", email:"lucia@gmail.com"
-},
-{
-    name:"andres", email:"andres@gmail.com"
-},
-{
-    name:"jorge", email:"jorge@gmail.com"
-},
-{
-    name:"laura",email: "laura@gmail.com"
-},
-{
-    name:"jose", email:"jose@gmail.com"
-},
-{
-    name:"jose", email:"jose@gmail.com"
-},
+    {
+        name: "pepito", email: "pepito@gmail.com", id: 1,
+    },
+    {
+        name: "lucia", email: "lucia@gmail.com", id: 2,
+    },
+    {
+        name: "andres", email: "andres@gmail.com", id: 3,
+    },
+    {
+        name: "jorge", email: "jorge@gmail.com", id: 4,
+    },
+    {
+        name: "laura", email: "laura@gmail.com", id: 5,
+    },
+    {
+        name: "jose", email: "jose@gmail.com", id: 6,
+    },
+    {
+        name: "jose", email: "jose@gmail.com", id: 7,
+    },
 ]
 
-formHTML.addEventListener("submit")
 
 
-function desplegarUsuarios(){
+
+
+//{ => {}}
+
+formHTML.addEventListener("submit", e => {
+    e.preventDefault()
+
+    postUser()
+
+
+})
+
+
+
+// var hijoAgregado = listaHTML.insertBefore(daniel, pepito);
+
+const htmlCollectionBtn = []
+
+function desplegarUsuarios() {
+    listaHTML.innerHTML = "";
     let lista = getUsuarios();
-    lista.map(e =>{
-        const item = document.createElement("li")
-        item.textContent = e.name
-        listaHTML.appendChild(item)
+
+    lista.map((user) => {
+        const item = document.createElement("li");
+        item.id = user.id;
+        item.style.display = "inline-block";
+
+        item.innerHTML = `
+        <div>
+            <h3>${user.name}</h3>
+            <small>${user.email}</small>
+            <b>ID: ${user.id}</b>
+        </div>
+        <div>
+            <button id=${user.id} class="eliminar" onclick="eliminarUsuario()">Eliminar</button>
+            <button onclick="editarUsuario(${user.id})">Editar</button>
+        </div>
+        `;
+
+        listaHTML.appendChild(item);
+    });
+
+    const btnDeleteCollection = document.getElementsByClassName("eliminar");
+    for (let i = 0; i < btnDeleteCollection.length; i++) {
+        const button = btnDeleteCollection[i];
+        button.addEventListener("click", () => {
+            eliminarUsuario(button.id)
+        });
     }
-    )
 }
 
-document.addEventListener("DOMContentLoaded",e => {
+
+
+
+document.addEventListener("DOMContentLoaded", e => {
     desplegarUsuarios()
 })
 
-function postUser(){
-    
-const emailInput = document.getElementById("email").value
-const nameInput = document.getElementById("name").value
-const idInput = document.getElementById("id").value
+function postUser() {
 
-    let nuevoUsuario = {
-        nombre : nameInput,
-        correo : emailInput,
-        id: idInput,
+    const emailInput = document.getElementById("email").value
+    const nameInput = document.getElementById("name").value
+    const idInput = document.getElementById("id").value
+    const idRepetido = listaUsuarios.find(e => e.id == idInput);
+    if (!idRepetido) {
+        let nuevoUsuario = {
+            name: nameInput,
+            email: emailInput,
+            id: idInput,
+        }
+        listaUsuarios.push(nuevoUsuario)
+        desplegarUsuarios()
+        return
     }
-    listaUsuarios.push(nuevoUsuario)
+    alert("el usuario esta repetido")
 
-    console.log(listaUsuarios);
+
 }
 
+function eliminarUsuario(id) {
 
-
-
+    const lista = getUsuarios()
+    const usuario = getUsuario(parseInt(id))
+    const indice = lista.indexOf(...usuario)
+    console.log(indice);
+    if (indice != -1) {
+        lista.splice(indice, 1);
+    } else {
+      console.warn("Usuario con ID", id, "no encontrado");
+    }
+    console.log(lista);
+    desplegarUsuarios()
+}
 
 
 // C R U D
@@ -75,14 +133,14 @@ const idInput = document.getElementById("id").value
 
 //declaradas
 
-function getUsuarios(){
+function getUsuarios() {
 
     return listaUsuarios;
 }
 
-function getUsuario(id){
-
-    let usuario = listaUsuarios.filter(e => e.id === id )
+function getUsuario(id) {
+    const lista = getUsuarios()
+    let usuario = lista.filter(e => e.id == id)
     return usuario;
 }
 
